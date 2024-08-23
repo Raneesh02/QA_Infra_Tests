d5059f602c33dd0bc313691cd9f43ec769308257
raneeshchoudhary <raneeshchoudhary@gmail.com>
2024-08-23 21:22:58 +0530

    awsexecution

--- src/main/java/Base/DriverManager.java
+++ src/main/java/Base/DriverManager.java
@@ -16,21 +16,22 @@ public class DriverManager {
 
     public static WebDriver initDriver()  {
         FirefoxOptions options = new FirefoxOptions();
-        options.addArguments("--headless");
+//        options.addArguments("--headless");
         options.addArguments("--disable-gpu");
         options.addArguments("--no-sandbox");
 
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--no-sandbox");
         chromeOptions.addArguments("--disable-dev-shm-usage");
-        chromeOptions.addArguments("--headless");
+//        chromeOptions.addArguments("--headless");
 
 //        threadLocalDriver.set(new ChromeDriver(chromeOptions));
         try {
-            threadLocalDriver.set(new RemoteWebDriver(new URL("http://localhost:4444"),chromeOptions));
+            threadLocalDriver.set(new RemoteWebDriver(new URL("http://a23e678ea555349409f2bf001b158f16-1466972497.ap-south-1.elb.amazonaws.com:4444"),chromeOptions));
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
+        threadLocalDriver.get().manage().window().maximize();
         return threadLocalDriver.get();
     }
 
